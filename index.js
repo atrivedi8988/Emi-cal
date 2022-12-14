62854735f3115e82224e98f847c4ac9e0d7ddf32
Aman Trivedi <101327752+atrivedi8988@users.noreply.github.com>
2022-12-15 00:24:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
-const UserModel = require("./users.Schema")
+const UserModel = require("./users.Schema.js")
 
 
 
